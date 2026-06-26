c4d2308aaa8e6d966bcc71ad1dd47120293dde7a
sowmya31206 <sowmyashanmugavel31206@gmail.com>
2026-06-26 07:44:29 +0530

    Create student_marks.py

---
+++ student_marks.py
@@ -0,0 +1,13 @@
+
+print("HSC Marks")
+marks = {
+    "Tamil":96,
+    "English":85,
+    "Maths":77,
+    "Physics":74,
+    "Chemistry":85,
+    "Computer Science":97
+}
+
+for subject, mark in marks.items():
+    print(subject, ":", mark)
