495d6aa4abf9ccef44293fc9deb5728f7bef512a
sowmya31206 <sowmyashanmugavel31206@gmail.com>
2026-06-26 07:16:18 +0530

    Create list_operations.py

---
+++ list_operations.py
@@ -0,0 +1,7 @@
+print("List operations")
+numbers = [10, 20, 30, 40, 50]
+
+print("List:",numbers)
+print("First element:",numbers[0])
+print("Last element:",numbers[-1])
+print("Length:",len(numbers))
