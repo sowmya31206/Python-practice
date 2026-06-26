db78c7764dedfbf4558864a48d2a1be3dcb3d6a4
sowmya31206 <sowmyashanmugavel31206@gmail.com>
2026-06-26 07:38:52 +0530

    Create dictionary_demo.py

---
+++ dictionary_demo.py
@@ -0,0 +1,10 @@
+student = {student = {
+    "name": "Sowmya",
+    "age": 20,
+    "department": "Computer Science"
+}
+
+print(student)
+print("Student name:",student["name"])
+print("Student age:",student["age"])
+print("Student department:",student["department"])
