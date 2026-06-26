82d176d420ce652ef6b876c04bc056c2cb4f7d58
sowmya31206 <sowmyashanmugavel31206@gmail.com>
2026-06-26 07:12:36 +0530

    Create factorial.py

---
+++ factorial.py
@@ -0,0 +1,10 @@
+print("Factorial")
+def factorial(num):
+    fact = 1
+    for i in range(1, num + 1):
+        fact = fact * i
+    return fact
+
+number = int(input("Enter a number: "))
+
+print("Factorial =",factorial(number))
